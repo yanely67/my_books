cb0f28695e765587d759559d5ba4754eb9ce9494
Gutierrez-Jostin <elenamarquez2000casilla@gmail.com>
2026-04-26 19:30:21 -0700

    cambios hechos

--- books/models.py
+++ books/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.title
+
+class Book(models.Model):
+
+    title = models.CharField(max_length=250)
+    author = models.CharField(max_length=250)
+    image = models.CharField(max_length=2000)
+    created_at =models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
--- books/views.py
+++ books/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Book
 
 # Create your views here.
+def home(request):
+    books = Book.objects.all()
+    return render(request, 'books/home.html', {'books': books})
+
+def detail_book(request, id):
+    book = get_object_or_404(Book, id=id)
+    return render(request, 'books/detail_book.html', {'detail_book': book})
+    
