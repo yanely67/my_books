8181c08fdf5608f182e84f9f251f99b528723edb
Gutierrez-Jostin <elenamarquez2000casilla@gmail.com>
2026-04-26 21:21:20 -0700

    Finaliza

--- books/models.py
+++ books/models.py
@@ -7,11 +7,11 @@ class Category(models.Model):
     def __str__(self):
         return self.title
 
-class Book(models.Model):
-
+class Books(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     title = models.CharField(max_length=250)
     author = models.CharField(max_length=250)
-    image = models.CharField(max_length=2000)
+    image = models.ImageField(max_length=2000)
     created_at =models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
 
--- books/views.py
+++ books/views.py
@@ -1,12 +1,21 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Book
+from django.db.models import Q
+from django.shortcuts import render, get_object_or_404, redirect
+
+from .models import Books, Category
 
 # Create your views here.
 def home(request):
-    books = Book.objects.all()
-    return render(request, 'books/home.html', {'books': books})
+    books = Books.objects.all()
+    context = {
+        'books': books
+    }
+    return render(request, 'books/home.html', context)
 
 def detail_book(request, id):
-    book = get_object_or_404(Book, id=id)
-    return render(request, 'books/detail_book.html', {'detail_book': book})
-    
+    book = get_object_or_404(Books, id=id)
+
+    context = {
+        'book': book,
+    }
+
+    return render(request, 'books/detail.html', context)    
